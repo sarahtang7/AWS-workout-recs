f1273146d9d060e6e6e9f2583f20d42b5b19fea8
Mansi Singh <52072175+mansion99@users.noreply.github.com>
2023-05-07 03:53:57 -0400

    added recommendations component

---
+++ src/components/recommendations/Recommendations.js
@@ -0,0 +1,14 @@
+import '@aws-amplify/ui-react/styles.css';
+
+const Recommendations = () => {
+
+    return (
+        <div>
+            <h1>Recommended Workouts for you!</h1>
+            <br></br>
+            <p>have to be done</p>
+        </div>
+    );
+}
+
+export default Recommendations;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import '@aws-amplify/ui-react/styles.css';
+import Recommendations from '../components/recommendations/Recommendations';
 
 const Home = () => {
 
     return (
         <div>
             <h1>Hello, welcome to Get Fit!</h1>
+            <Recommendations/>
         </div>
     );
 }
